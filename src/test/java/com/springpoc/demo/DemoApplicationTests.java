54877b880b9e5171960a51c649f48596ef6efe2d
shijo-jack <55213452+shijo-jack@users.noreply.github.com>
2019-10-22 15:05:21 +0200

    Added a comment for test case

--- src/test/java/com/springpoc/demo/DemoApplicationTests.java
+++ src/test/java/com/springpoc/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.springpoc.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Test cases to be added in future
+
 @SpringBootTest
 class DemoApplicationTests {
 
